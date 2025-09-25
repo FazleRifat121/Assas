153473681e56e89fe5c2a99b33ba8030d0a813fe
Fazle Rifat <fazlerifatofficial@gmail.com>
2025-09-26 01:27:20 +0600

    Update HeroFour.jsx

--- src/pages/Home/Hero/HeroFour.jsx
+++ src/pages/Home/Hero/HeroFour.jsx
@@ -4,7 +4,7 @@ const HeroFour =()=>{
         <>
             <div className="herofour h-[50vh]">
                 <h1 className="text-center text-3xl">Together for a <br /><span className="text-[#22B573]">Healthier</span> Tomorrow</h1>
-                <h4 className="text-center text-lg mt-60">At <span className="text-[#22B573]">Asshash Health</span>, we believe health is more than treatment <br />
+                <h4 className="text-center text-lg mt-60 hidden lg:block">At <span className="text-[#22B573]">Asshash Health</span>, we believe health is more than treatment <br />
 it’s a balance of body, mind, and spirit.
 </h4>
             </div>
