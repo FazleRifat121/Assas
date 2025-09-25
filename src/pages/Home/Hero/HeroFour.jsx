3d62abd9c50e0c37c72f59489b06ac605125fbd4
Fazle Rifat <fazlerifatofficial@gmail.com>
2025-09-25 22:28:31 +0600

    Hero four page added

---
+++ src/pages/Home/Hero/HeroFour.jsx
@@ -0,0 +1,14 @@
+const HeroFour =()=>{
+
+    return(
+        <>
+            <div className="herofour h-[50vh]">
+                <h1 className="text-center text-3xl">Together for a <br /><span className="text-[#22B573]">Healthier</span> Tomorrow</h1>
+                <h4 className="text-center text-lg mt-60">At <span className="text-[#22B573]">Asshash Health</span>, we believe health is more than treatment <br />
+it’s a balance of body, mind, and spirit.
+</h4>
+            </div>
+        </>
+    )
+}
+export default HeroFour;
