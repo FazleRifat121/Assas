9c7a937595d4afa201ad5c035941b9aa9d180bde
Fazle Rifat <fazlerifatofficial@gmail.com>
2025-09-25 22:30:07 +0600

    FAQ changed

--- src/pages/Home/ClientReviews/Faq.jsx
+++ src/pages/Home/ClientReviews/Faq.jsx
@@ -6,22 +6,22 @@ import { useState } from "react";
 
 const faqData = [
   {
-    question: "Who can invest with Digiyog?",
+    question: "How does the Health Card work?",
     answer:
       "Anyone with a Bangladeshi bank account or MFS (Mobile Financial Services) account can invest in fractionalized <br /> real estate properties through Digiyog.",
   },
   {
-    question: "Is my personal information secure?",
+    question: "Can patients without smartphones get help via an agent?",
     answer:
       "We use advanced cybersecurity technologies and strict access controls to protect user data stored in the cloud.",
   },
   {
-    question: "Can I visit the property myself?",
+    question: "How is privacy protected?",
     answer:
       "Yes! We provide property details and encourage you to visit the properties in person.",
   },
   {
-    question: "What is the minimum investment amount on Digiyog?",
+    question: "Supported payment methods",
     answer: "The minimum investment is BDT 500.",
   },
 ];
@@ -47,12 +47,12 @@ const Faq = () => {
   }, []);
   return (
     <div className="relative my-5 lg:my-0 p-4 ">
-      <div className="container mx-auto p-4 bg-green-800 rounded-3xl relative z-10">
+      <h1 className="text-3xl text-center mb-4">Frequently Asked Questions</h1>
+      <div className="container mx-auto p-4 bg-[#D9D9D9] rounded-3xl relative z-10">
+        
         <div className="flex flex-col items-center justify-around lg:flex-row ">
+          
           <div className="lg:w-52 text-center  mt-20">
-            <h1 className="text-6xl lg:text-4xl 2xl:text-6xl text-white font-bold leading-15 ">
-              Frequently <span className="xl:ml-12">Asked</span> Questions
-            </h1>
           </div>
           <div className="mt-20" id="faqCards">
             {faqData.map((faq, index) => (
@@ -80,14 +80,7 @@ const Faq = () => {
               </div>
             ))}
 
-            <Link
-              to="/frequent"
-              onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-              className="text-[#8EC63F] flex justify-end items-center"
-            >
-              <p>View More</p>
-              <IoMdArrowForward className="mt-[3px]" />
-            </Link>
+          
           </div>
         </div>
       </div>
